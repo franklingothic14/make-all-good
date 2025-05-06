96ca1d059eca2117a2f4de4c2b398be2c51fe69a
franklingothic14 <46635211+franklingothic14@users.noreply.github.com>
2025-05-06 22:06:02 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
 const button = document.getElementById('main-button');
 const body = document.body;
 
-button.addEventListener('click', (e) => {
-  const ripple = document.createElement('div');
-  ripple.id = 'ripple';
-  document.body.appendChild(ripple);
-
-  const rect = body.getBoundingClientRect();
-  const maxDim = Math.max(rect.width, rect.height);
-  const rippleSize = maxDim * 2;
-
-  const x = e.clientX - rippleSize / 2;
-  const y = e.clientY - rippleSize / 2;
-
-  ripple.style.width = ripple.style.height = rippleSize + 'px';
-  ripple.style.left = x + 'px';
-  ripple.style.top = y + 'px';
-
+button.addEventListener('click', () => {
+  button.classList.add('clicked');
   setTimeout(() => {
-    ripple.style.transform = 'scale(1)';
-  }, 10);
-
-  setTimeout(() => {
-    body.classList.remove('dark');
-    body.classList.add('light');
     button.textContent = 'Все добре';
     button.disabled = true;
-  }, 600);
-
-  setTimeout(() => {
-    ripple.remove();
-  }, 1500);
+    body.classList.remove('dark');
+    body.classList.add('light');
+  }, 300); // даємо час для анімації кнопки
 });
