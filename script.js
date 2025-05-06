a6cd500618d3ac9769d593040830b45396200088
franklingothic14 <46635211+franklingothic14@users.noreply.github.com>
2025-05-06 21:48:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const button = document.getElementById('main-button');
 const body = document.body;
 
 button.addEventListener('click', () => {
-  button.textContent = 'Все добре';
-  button.disabled = true;
-  body.classList.remove('dark');
-  body.classList.add('light');
+  button.classList.add('clicked');
+  setTimeout(() => {
+    button.textContent = 'Все добре';
+    button.disabled = true;
+    body.classList.remove('dark');
+    body.classList.add('light');
+  }, 300); // даємо час для анімації кнопки
 });
